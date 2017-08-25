53f1f1347e2c65c353050b33e6e8e1259cd5371f
tempex <iqcooler@gmail.com>
2017-08-25 22:32:49 +0800

    Initial commit

---
+++ app/src/main/java/com/example/iqbal/lab1inputouput/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.iqbal.lab1inputouput;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvMessage;
+    Button submitButton;
+
+    int counter=0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvMessage = (TextView) findViewById(R.id.textView);
+        submitButton = (Button) findViewById(R.id.button);
+
+    }
+
+    public void displayText (View view){
+
+        //when the button is clicked, do something here
+        tvMessage.setText("Welcome! - " + counter++);
+
+        //show toast
+        Toast toast = Toast.makeText(this,"WELCOME", Toast.LENGTH_SHORT);
+        toast.show();
+
+    }
+
+}
